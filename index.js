42011a0fa842b107574b846a12cbed456c1e37d5
h1m <himanshu.yadav643@gmail.com>
2021-04-08 20:01:44 +0530

    starting with oauth authentication with passport

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
+require("./services/passport");
+// const authRoutes = require("./routes/authRoutes");
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+// authRoutes(app);
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
