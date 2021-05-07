04303380a8a1186e34b8652db9d53fe7907a139b
h1m <himanshu.yadav643@gmail.com>
2021-05-07 15:21:02 +0530

    refactored passport.js file and started working on the client side

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,15 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true,
     },
-    (accessToken, refreshToken, profile, done) => {
+    async (accessToken, refreshToken, profile, done) => {
       // console.log("Access Token", accessToken);
       // console.log("Profile", profile);
-      User.findOne({ googleId: profile.id }).then((existingUser) => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id }).save().then((user) => {
-            done(null, user);
-          });
-        }
-      });
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = await new User({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
