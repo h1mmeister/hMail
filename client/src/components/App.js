47c78c3779ea6fafdd065837262f736603100823
h1m <himanshu.yadav643@gmail.com>
2021-05-09 16:07:02 +0530

    added react router and materialize css

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Header from "./Header";
+
+const Dashboard = () => <div>Dashboard</div>;
+const SurveyNew = () => <div>SurveyNew</div>;
+const Landing = () => <div>Landing</div>;
 
 const App = () => {
-  return <div>Hi There</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={Landing} />
+          <Route path="/surveys" exact component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,5 @@
+import materializeCSS from "materialize-css/dist/css/materialize.min.css";
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
