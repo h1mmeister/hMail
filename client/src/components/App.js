38a3afc3b84f4f4cc65390f47016e07aa853d6b2
h1m <himanshu.yadav643@gmail.com>
2021-05-12 03:52:15 +0530

    working on the client side, actions/reducers

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
+import { connect } from "react-redux";
+import * as actions from "../actions";
 
 const Dashboard = () => <div>Dashboard</div>;
 const SurveyNew = () => <div>SurveyNew</div>;
 const Landing = () => <div>Landing</div>;
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route path="/" exact component={Landing} />
-          <Route path="/surveys" exact component={Dashboard} />
-          <Route path="/surveys/new" component={SurveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path="/" exact component={Landing} />
+            <Route path="/surveys" exact component={Dashboard} />
+            <Route path="/surveys/new" component={SurveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(null, actions)(App);
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,20 @@ import { React, Component } from "react";
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">
+            <i></i>hMail
+          </a>
+          <ul className="right">
+            <li>
+              <a>Sign In with Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
