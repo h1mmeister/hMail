6be30c6a468fcf48c1979e2a8263d7a3df6bf662
h1m <himanshu.yadav643@gmail.com>
2021-05-23 13:38:19 +0530

    payment handling complete

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { Redirect } from "react-router";
 import { FETCH_USER } from "./types";
 
 // export const fetchUser = () => {
--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const { Schema } = mongoose; // const Schema = mongoose.Schema
 
 const userSchema = new Schema({
   googleId: String,
+  credits: { type: Number, default: 0 },
 });
 
 // Model Class
