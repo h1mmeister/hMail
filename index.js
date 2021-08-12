2b514e203c52eb1f43d4be98c94061639f26bdf6
h1m <himanshu.yadav643@gmail.com>
2021-08-12 19:53:42 +0530

    dynamic port binding for Heroku

--- index.js
+++ index.js
@@ -34,5 +34,6 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+// dynamic port binding for heroku
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
