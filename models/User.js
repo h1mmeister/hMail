fce3e4518914f54e48f75427fdf77327bc9490b7
h1m <himanshu.yadav643@gmail.com>
2021-08-16 20:25:47 +0530

    refactoring the models class

--- models/User.js
+++ models/User.js
@@ -1,10 +1,12 @@
+// requiring the libraries
 const mongoose = require("mongoose");
 const { Schema } = mongoose; // const Schema = mongoose.Schema
 
+// creating the schema for the records
 const userSchema = new Schema({
   googleId: String,
   credits: { type: Number, default: 0 },
 });
 
-// Model Class
+// creating the model class
 mongoose.model("users", userSchema);
