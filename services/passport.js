82a1aa89a74b027ccf17aefac328c471147d4b21
h1m <himanshu.yadav643@gmail.com>
2021-08-16 20:38:20 +0530

    refactoring the passport.js file

--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,7 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const keys = require("../config/keys");
 const mongoose = require("mongoose");
 
+// one argument means we are trying to fetch something out of mongoose - NOT creating the model class here
 const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
