c3aba5156364a8a9913b66f62cc59d2204e9f1a0
h1m <himanshu.yadav643@gmail.com>
2021-08-28 17:33:51 +0530

    relationship betwwen user and survey collections

--- models/Survey.js
+++ models/Survey.js
@@ -1,15 +1,19 @@
 // requiring the libraries
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const RecipientSchema = require("./Recipient");
 
 // creating the schema for the surveys
 const surveySchema = new Schema({
   title: String,
   body: String,
   subject: String,
-  recipients: [String],
+  recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
   no: { type: Number, default: 0 },
+  _user: { type: Schema.Types.ObjectId, ref: "User" },
+  dateSent: Date,
+  lastResponded: Date,
 });
 
 // creating the model class
