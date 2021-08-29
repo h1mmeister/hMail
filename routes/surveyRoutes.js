c81b1fe957f521de05e6ec8baa7256f94d9b92e9
h1m <himanshu.yadav643@gmail.com>
2021-08-30 01:32:27 +0530

    checking whether the user is logged in to create a survey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,1 +1,5 @@
-module.exports = (app) => {};
+const requireLogin = require("../middlewares/requireLogin");
+
+module.exports = (app) => {
+  app.post("/api/surveys", requireLogin, (req, res) => {});
+};
