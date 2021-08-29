3ec02ca466c90ddfff0689eb73a147aafdba2772
h1m <himanshu.yadav643@gmail.com>
2021-08-30 01:49:31 +0530

    creating the instance of the survey

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,5 +1,18 @@
+const mongoose = require("mongoose");
+const requireCredits = require("../middlewares/requireCredits");
 const requireLogin = require("../middlewares/requireLogin");
 
+const Survey = mongoose.model("surveys");
+
 module.exports = (app) => {
-  app.post("/api/surveys", requireLogin, (req, res) => {});
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipients } = req.body;
+
+    // creating the instance of the survey
+    const survey = Survey({
+      title,
+      subject,
+      body,
+    });
+  });
 };
